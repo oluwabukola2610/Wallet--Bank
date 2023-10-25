87d6d3a21b7c42a2eafb68a15c7b462a75c9e6d0
oluwabukola2610 <roheemohmudashir@gmail.com>
2023-10-25 12:24:00 +0100

    new added functionality

--- src/component/FundsForm.jsx
+++ src/component/FundsForm.jsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import useHandleFunds from "../Hook/useHandleFunds";
-
+import { FaRegCopy } from "react-icons/fa";
+import { useState } from "react";
 const FundsForm = () => {
   const {
     selectedCurrency,
@@ -14,10 +15,39 @@ const FundsForm = () => {
   const handleInput = (event) => {
     setFundsInput(event.target.value);
   };
-  // const [warningMessage, setWarningMessage] = useState(false);
+  const [showCopiedMessage, setShowCopiedMessage] = useState(false);
+  const cardNumber = "4242424242424242";
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(cardNumber);
+      setShowCopiedMessage(true);
+      setTimeout(() => setShowCopiedMessage(false), 2000);
+    } catch (error) {
+      console.error("Failed to copy text: ", error);
+    }
+  };
+
   return (
     <dialog id="my_modal_3" className="modal">
       <div className="modal-box">
+        {showCopiedMessage && (
+          <div className="alert alert-success">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Text copied to clipboard</span>
+          </div>
+        )}
         <h2 className="text-2xl p-4 border-b border-b-slate-300 shadow-sm">
           Fund Wallet
         </h2>
@@ -117,6 +147,19 @@ const FundsForm = () => {
           )}
           {selectedCurrency === "usd" && (
             <div className=" mt-3">
+              <div className="space-y-3">
+                <p className="text-sm ">
+                  To continue with your funding process, kindly copy the card
+                  number below and use any number for your CVV and exp date.
+                </p>
+                <span className="flex justify-between font-normal bg-[#2488FF80] p-2 rounded-lg">
+                  <p className="text-[0.75rem] font-semibold">{cardNumber}</p>
+                  <button onClick={handleCopyClick}>
+                    <FaRegCopy />
+                  </button>
+                </span>
+              </div>
+
               <button
                 onClick={fundWithStripe}
                 className="bg-blue-500 text-white rounded-lg py-2 px-4 mt-2"
